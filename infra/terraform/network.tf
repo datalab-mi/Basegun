7faf6c392616254b43b25f34e3cd5d26044209e4
titigmr <thierry.gmr@outlook.fr>
2022-05-28 23:39:21 +0200

    add router

--- infra/terraform/network.tf
+++ infra/terraform/network.tf
@@ -13,6 +13,11 @@ data "openstack_networking_network_v2" "internal_network" {
 resource "openstack_networking_subnet_v2" "subnet" {
   network_id = data.openstack_networking_network_v2.internal_network.id
   cidr       = var.subnet_cidr
+  gateway_ip = cidrhost(var.subnet_cidr, 5)
+  allocation_pool {
+    start      = cidrhost(var.subnet_cidr, 10)
+    end        = cidrhost(var.subnet_cidr, -2)
+  }
 }
 
 # Use keypair
@@ -20,13 +25,14 @@ data "openstack_compute_keypair_v2" "keypair" {
   name = "dev-keypair-tf"
 }
 
-# Create router attached on external network and internal subnet
-resource "openstack_networking_router_v2" "router" {
-  name                = "router"
-  external_network_id = data.openstack_networking_network_v2.external_network.id
-}
+# NOT AVAILABLE
+# # Create router attached on external network and internal subnet
+# resource "openstack_networking_router_v2" "router" {
+#   name                = "router"
+#   external_network_id = data.openstack_networking_network_v2.external_network.id
+# }
 
-resource "openstack_networking_router_interface_v2" "router_interface" {
-  router_id = "${openstack_networking_router_v2.router.id}"
-  subnet_id = "${openstack_networking_subnet_v2.subnet.id}"
-}
+# resource "openstack_networking_router_interface_v2" "router_interface" {
+#   router_id = "${openstack_networking_router_v2.router.id}"
+#   subnet_id = "${openstack_networking_subnet_v2.subnet.id}"
+# }
