11c2b65bfbddc5f68cf8e2256334a86c5289a824
titigmr <thierry.gmr@outlook.fr>
2022-05-29 01:03:50 +0200

    add router

--- infra/terraform/network.tf
+++ infra/terraform/network.tf
@@ -25,14 +25,14 @@ data "openstack_compute_keypair_v2" "keypair" {
   name = "dev-keypair-tf"
 }
 
-# NOT AVAILABLE
-# # Create router attached on external network and internal subnet
-# resource "openstack_networking_router_v2" "router" {
-#   name                = "router"
-#   external_network_id = data.openstack_networking_network_v2.external_network.id
-# }
 
-# resource "openstack_networking_router_interface_v2" "router_interface" {
-#   router_id = "${openstack_networking_router_v2.router.id}"
-#   subnet_id = "${openstack_networking_subnet_v2.subnet.id}"
-# }
+ # Create router attached on external network and internal subnet
+ resource "openstack_networking_router_v2" "router" {
+   name                = "router"
+   external_network_id = data.openstack_networking_network_v2.external_network.id
+ }
+
+ resource "openstack_networking_router_interface_v2" "router_interface" {
+   router_id = "${openstack_networking_router_v2.router.id}"
+   subnet_id = "${openstack_networking_subnet_v2.subnet.id}"
+ }
